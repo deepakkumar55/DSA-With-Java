776214ef4c45e14ad7e631fd6b70a17bb0f6f42b
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2023-08-13 04:56:58 +0530

    Update multiDArrayList.java

--- src/Array/multiDArrayList.java
+++ src/Array/multiDArrayList.java
@@ -1,7 +1,9 @@
 package Array;
 
+import java.util.ArrayList;
+
 public class multiDArrayList {
     public static void main(String[] args) {
-                
+        ArrayList<ArrayList<Integer>>        
     }
 }

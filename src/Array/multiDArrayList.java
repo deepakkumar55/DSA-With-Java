a1419dd5c1797e99507ddf2b99b1ee07268d8c05
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2023-08-13 04:57:51 +0530

    Update multiDArrayList.java

--- src/Array/multiDArrayList.java
+++ src/Array/multiDArrayList.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 
 public class multiDArrayList {
     public static void main(String[] args) {
-        ArrayList<ArrayList<Integer>>        
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            list.add(i);
+            
+        }      
     }
 }

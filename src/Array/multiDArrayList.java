a736569a98da7714bc91139aa75fe2454f9a5be3
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2023-08-13 05:06:29 +0530

    Update multiDArrayList.java

--- src/Array/multiDArrayList.java
+++ src/Array/multiDArrayList.java
@@ -1,13 +1,25 @@
 package Array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class multiDArrayList {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            list.add(i);
-            
-        }      
+        for (int i = 0; i < 2; i++) {
+            list.add(new ArrayList<>());
+
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                list.get(i).add(sc.nextInt());
+            }
+        }
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
     }
 }

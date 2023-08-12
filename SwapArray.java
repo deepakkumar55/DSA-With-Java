54bc03851e131b7241f3cc92b9ae6bf8d7325aef
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2023-08-13 05:26:45 +0530

    Update SwapArray.java

--- SwapArray.java
+++ SwapArray.java
@@ -1,8 +1,15 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SwapArray {
     public static void main(String[] args) {
         int[] arr ={1,2,5,32,112,332,45,677,8865,222,973};
-        swap
+        swap(arr,1,9);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void swap(int[] arr, int i,int k){
+        int temp= arr[i];
+        arr[i]=arr[k];
+        arr[k] =temp ;
     }
 }

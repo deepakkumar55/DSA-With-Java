eaedc68a4f6e0763ab81b9782ff62129a03bc359
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2023-08-13 05:22:26 +0530

    Create SwapArray.java

---
+++ SwapArray.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class SwapArray {
+    public static void main(String[] args) {
+        int[] arr ={1,2,5,32,112,332,45,677,8865,222,973};
+        swap
+    }
+}

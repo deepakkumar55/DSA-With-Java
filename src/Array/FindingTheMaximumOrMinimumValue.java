507257d96414c4c2082da932fc0432bafa5de1e9
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2023-08-13 05:37:26 +0530

    Update FindingTheMaximumOrMinimumValue.java

--- src/Array/FindingTheMaximumOrMinimumValue.java
+++ src/Array/FindingTheMaximumOrMinimumValue.java
@@ -1,7 +1,39 @@
 package Array;
 
+import java.util.Scanner;
+
 public class FindingTheMaximumOrMinimumValue {
     public static void main(String[] args) {
+    int[] arr={
+        1,23,-40,52
+    };
+    System.out.println(max(arr));
+    System.out.println(min(arr));
+}
+    static int max(int[] arr) {
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            
+        }
+        return max;
+        
         
     }
+    static int min(int[] arr) {
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            
+        }
+        return min;
+        
+    }
+    
 }

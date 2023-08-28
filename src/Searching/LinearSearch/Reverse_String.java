483e3c7afe58cd083acd1175654e7c44d4613cad
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2023-08-29 00:58:51 +0530

    Create Reverse_String.java

---
+++ src/Searching/LinearSearch/Reverse_String.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Reverse_String {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        for (int i = 0; i < args.length; i++) {
+            
+        }
+    }
+}
